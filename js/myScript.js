0b97a6193cc439f51b726ca7f51ede34bdf8fa01
NhuYLe <lenhuy@sheridancollege.ca>
2023-04-28 00:28:24 -0400

    first commit

---
+++ js/myScript.js
@@ -0,0 +1,57 @@
+var sData;
+var academic = new Array();
+
+class Student {
+  constructor(name, program, title, homeCountry, profileImage, aboutMe) {
+    this.name = name;
+    this.program = program;
+    this.title = title;
+    this.homeCountry = homeCountry;
+    this.profileImage = profileImage;
+    this.aboutMe = aboutMe;
+  }
+}
+function loadData() {
+  fetch("./dataFiles/data.json")
+    .then((res) => res.json())
+    .then((dataJSON) => {
+      console.log("this is data");
+      console.log(dataJSON);
+      showData(dataJSON);
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+function showData(dataJSON) {
+  stu = dataJSON.student;
+  sData = new Student(
+    stu.name,
+    stu.program,
+    stu.title,
+    stu.homeCountry,
+    stu.profileImage,
+    stu.aboutMe
+  );
+  document.getElementById("myName").innerHTML = `${sData.name}`;
+  document.getElementById("title").innerHTML = `${sData.title}`;
+  document.getElementById("picture").src = `${sData.profileImage}`;
+  document.getElementById("aboutMe").innerHTML = `${sData.aboutMe}`;
+}
+
+class academicYear {
+  constructor(semester, transcript) {
+    this.semester = semester;
+    this.transcript = transcript;
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "60%";
+  document.getElementById("mySIdebar").style.width = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+}
